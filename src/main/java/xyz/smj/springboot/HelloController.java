1bc878657b5f41339834dc50bcc71cc23e3763c3
S. M. JAHANGIR <smjxpro@gmail.com>
2020-08-26 13:53:54 +0600

    Create a Simple Web Application

---
+++ src/main/java/xyz/smj/springboot/HelloController.java
@@ -0,0 +1,12 @@
+package xyz.smj.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
